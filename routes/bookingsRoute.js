71b3c9e879a1a99c514340cd4e1ed5d990683bcc
Shawn Kebel <fxrender@outlook.com>
2022-02-13 02:35:45 -0600

    :construction::sparkles: Work on `car booking` backend

---
+++ routes/bookingsRoute.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const Booking = require("../models/bookingModel");
+const Car = require("../models/carModel");
+const { v4: uuidv4 } = require("uuid");
+
+router.post('/bookcar', async(req, res) => {
+  req.body.transactionId = '1234'
+  try {
+    const newbooking = new Booking(req.body);
+    await newbooking.save();
+    const car = await Car.findOne({_id : req.body.car.toString()});
+    car.bookedTimeSlots.push(req.body.bookedTimeSlots);
+
+    await car.save();
+    res.send('Your booking was successfully saved.')
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use('/api/cars/', require('./routes/carsRoute'));
 
 app.use('/api/users/', require('./routes/usersRoute'));
 
+app.use('/api/bookings/', require('./routes/bookingsRoute'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Node JS Server listening on port ${port}!`));
